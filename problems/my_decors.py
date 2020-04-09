e67b9376cbe5741665fd96d22cf60c9918c92f54
cnh867 <lukasz.osuch@motorolasolutions.com>
2020-04-10 01:39:04 +0200

    countit decorator added to my_decors module. It counts how many time a function has been invoked.

--- problems/my_decors.py
+++ problems/my_decors.py
@@ -11,3 +11,12 @@ def timeit(foo: callable) -> callable:
         print(f'\n{foo.__name__} took {took:.3f} sec\n')
         return outcome
     return wrapper_stopwatch
+
+
+def countit(foo: callable) -> callable:
+    @functools.wraps(foo)
+    def wrapper_countit(*args, **kwargs):
+        wrapper_countit.count += 1
+        return foo(*args, **kwargs)
+    wrapper_countit.count = 0
+    return wrapper_countit
