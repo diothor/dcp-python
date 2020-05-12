55f70c103fbee76ec84f6c0868dbe30d948aeb88
cnh867 <lukasz.osuch@motorolasolutions.com>
2020-05-12 21:55:48 +0200

    Problem #255 - O(n)

--- problems/problem_255.py
+++ problems/problem_255.py
@@ -1,12 +1,36 @@
-def josephus(n: int, k: int) -> int:
-    ppl = [p for p in range(1, n + 1)]
-    kill_place = k - 1
+class Person:
+    def __init__(self, value):
+        self.value = value
+        self.prev = None
+        self.next = None
 
-    # O(n-1)*O(n) ~= O(n^2)
-    while len(ppl) > 1:  # O(n-1)
-        del(ppl[kill_place])  # O(n)
-        kill_place = (kill_place + k - 1) % len(ppl)  # O(1)
-    return ppl.pop()
+    def __repr__(self):
+        return f'v:{self.value}'
+
+    def jump(self, steps):
+        res = self.next
+        for i in range(1, steps):
+            res = res.next
+            if res is None:
+                return res
+        else:
+            return res
+
+
+# O(2n) + O(n-1) ~= O(n)
+def josephus(n: int, k: int) -> int:
+    # O(2n)
+    ppl = [Person(p) for p in range(1, n + 1)]  # O(n)
+    for c, n in zip(ppl, ppl[1:]):  # O(n)
+        c.next, n.prev = n, c
+    ppl[0].prev, ppl[-1].next = ppl[-1], ppl[0]
+
+    to_kill = ppl[k-1]
+    # O(n-1)
+    while to_kill != to_kill.next:  # O(n-1)
+        to_kill.prev.next, to_kill.next.prev = to_kill.next, to_kill.prev  # O(1)
+        to_kill = to_kill.jump(k)  # O(1)
+    return to_kill.value
 
 
 assert josephus(5, 2) == 3
