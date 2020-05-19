5b17bdd8a783c6d08bad9e3db7e03c0ca8276ef2
cnh867 <lukasz.osuch@motorolasolutions.com>
2020-05-19 14:52:31 +0200

    Problem #309 - O(n*k)

--- problems/problem_309.py
+++ problems/problem_309.py
@@ -1,24 +1,38 @@
+from itertools import zip_longest
+
+
 def all_together_cost(seats: list) -> int:
     # n - number of seats equal to len(seats)
-    # k - number of people equal to seats.count(1)
-    zeros = []
-    ones = []
-    # O(n)
-    for i in range(len(seats)):  # O(n)
-        (ones if seats[i] == 1 else zeros).append(i)  # O(1)
+    n = len(seats)
 
+    ones_out = {i for i in range(n) if seats[i] == 1}  # O(n)
+    # k - number of people equal to seats.count(1)
+    k = len(ones_out)
+
+    zeros_in = set()
+    for i in range(k):  # O(k)
+        if seats[i] == 1:
+            ones_out.remove(i)
+        else:
+            zeros_in.add(i)
+
+    # O(n-k) * O(k) ~= O(n*k)
     hops = float('inf')
-    # O(n) * (2*O(n) + O(k)) ~= O(n^2)
-    for i in range(len(seats) - len(ones) + 1):  # O(n)
-        window_start = i
-        window_end = i + len(ones) - 1
-        zeros_in = list(filter(lambda x: window_start <= x < window_end, zeros))  # O(n)
-        ones_out = list(filter(lambda x: x < window_start or x > window_end, ones))  # O(n)
-        if len(zeros_in) == len(ones_out):
-            hops = min(hops, sum([abs(a - b) for a, b in zip(zeros_in, ones_out)]))  # O(k)
+    for i in range(k, n):  # O(n-k)
+        hops = min(hops, sum([abs(a - b) for a, b in zip_longest(zeros_in, ones_out, fillvalue=0)]))  # O(k)
+
+        if seats[i-k] == 0:
+            zeros_in.remove(i-k)  # O(1)
+        else:
+            ones_out.add(i-k)  # O(1)
+
+        if seats[i] == 0:
+            zeros_in.add(i)  # O(1)
+        else:
+            ones_out.remove(i)  # O(1)
     return hops
 
 
-assert all_together_cost([1, 0, 1, 0, 1]) == 3
+assert all_together_cost([1, 0, 1, 0, 1]) == 2
 assert all_together_cost([1, 0, 1, 0, 1, 1]) == 3
 assert all_together_cost([0, 1, 1, 0, 1, 0, 0, 0, 1]) == 5
