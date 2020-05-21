7e1b12590bf2e59e2849ec46cbefd7113aaa4026
cnh867 <lukasz.osuch@motorolasolutions.com>
2020-05-21 21:04:09 +0200

    Problem #314 fixed - O(n+k)

--- problems/problem_314.py
+++ problems/problem_314.py
@@ -1,23 +1,25 @@
 def broadcast_range(listeners: list, towers: list) -> int:
     min_range = 0
 
-    prev_tower = None
+    prev_tower = -float('inf')
     next_tower = towers[0]
     tower_index = 0
 
-    # O(n) where n is number of listeners equal to len(listeners)
+    # n is number of listeners equal to len(listeners)
+    # k is number of  towers equal to len(towers)
+    # O(n+k)
     for listener in listeners:
-        if listener > next_tower:
+        while listener > next_tower:
             prev_tower = next_tower
             tower_index += 1
-            next_tower = towers[tower_index] if tower_index < len(towers) else None
+            next_tower = towers[tower_index] if tower_index < len(towers) else float('inf')
 
-        dist_to_prev = listener - prev_tower if prev_tower is not None else float('inf')
-        dist_to_next = next_tower - listener if next_tower is not None else float('inf')
-        range_to_listener = min(dist_to_prev, dist_to_next)
-        min_range = max(min_range, range_to_listener)
-
+        dist_to_list = min(listener - prev_tower,  next_tower - listener)
+        min_range = max(min_range, dist_to_list)
     return min_range
 
 
+assert broadcast_range([0], [2]) == 2
+assert broadcast_range([2], [1, 2, 3]) == 0
+assert broadcast_range([4], [1, 2, 3]) == 1
 assert broadcast_range([1, 5, 11, 20], [4, 8, 15]) == 5
