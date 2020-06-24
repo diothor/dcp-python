85ca2742805c3e527b208b77a44b96e42e22a6cd
cnh867 <lukasz.osuch@motorolasolutions.com>
2020-06-25 00:03:13 +0200

    Problem #347 - fixed

--- problems/problem_347.py
+++ problems/problem_347.py
@@ -1,11 +1,22 @@
 # O(n)
 def smallex(s: str, k: int) -> str:
-    if k > 1:
-        return ''.join(sorted(s))
-    elif s[0] > s[1]:
-        return smallex(s[1:] + s[0], 1)
-    else:
+    if len(s) < 2:
         return s
+    elif k > 1:
+        return ''.join(sorted(s))
+
+    lowest = 0
+    for i in range(1, len(s)):
+        if s[i] < s[lowest]:
+            lowest = i
+        elif s[i] == s[lowest]:
+            j = 1
+            while s[i + j] == s[lowest + j]:
+                j += 1
+            else:
+                lowest = i if s[i + j] < s[lowest + j] else lowest
+    else:
+        return s[lowest:] + s[:lowest]
 
 
 assert smallex('bac', 2) == 'abc'
@@ -13,5 +24,9 @@ assert smallex('ggggaaaccbb', 2) == 'aaabbccgggg'
 assert smallex('gaurang', 3) == 'aaggnru'
 assert smallex('uagrang', 3) == 'aaggnru'
 
+assert smallex('kuh', 1) == 'hku'
+assert smallex('kuhyhx', 1) == 'hxkuhy'
 assert smallex('uagrang', 1) == 'agrangu'
 assert smallex('daily', 1) == 'ailyd'
+
+assert smallex('a', 7) == 'a'
