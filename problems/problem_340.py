5f82cf0c5dad6c8aace772bec052ecdedaef01d2
cnh867 <lukasz.osuch@motorolasolutions.com>
2020-07-10 00:11:16 +0200

    Problem #340 - test cases from geeksforgeeks.com added

--- problems/problem_340.py
+++ problems/problem_340.py
@@ -1,6 +1,6 @@
 # O(n^2) where n is number of points
-def closest(*points) -> list:
-    res_pair = []
+def closest(*points) -> set:
+    res_pair = set()
     min_dist = float('inf')
     for i in range(len(points)):
         for j in range(i + 1, len(points)):
@@ -8,9 +8,10 @@ def closest(*points) -> list:
             new_dist = abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
             if new_dist < min_dist:
                 min_dist = new_dist
-                res_pair = [p1, p2]
+                res_pair = {p1, p2}
     else:
-        return sorted(res_pair)
+        return res_pair
 
 
-assert closest((1, 1), (-1, -1), (3, 4), (6, 1), (-1, -6), (-4, -3)) == [(-1, -1), (1, 1)]
+assert closest((1, 1), (-1, -1), (3, 4), (6, 1), (-1, -6), (-4, -3)) == {(-1, -1), (1, 1)}
+assert closest((2, 3), (12, 30), (40, 50), (5, 1), (12, 10), (3, 4)) == {(2, 3), (3, 4)}
