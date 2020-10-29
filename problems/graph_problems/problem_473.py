c80510445a694afe2c2350ce6e48c2a791fdc3bb
cnh867 <lukasz.osuch@motorolasolutions.com>
2020-10-29 22:53:28 +0100

    Problem #473 - O(k)

--- problems/graph_problems/problem_473.py
+++ problems/graph_problems/problem_473.py
@@ -1,11 +1,12 @@
 from typing import List
 
 
+# O(k) -> v: vertices, e: edges from a single vertex, k = v*e: all edges in graph
 def transpose_graph(adjacency_list: List[List[int]]) -> List[List[int]]:
-    tgraph = [[] for _ in range(len(adjacency_list))]
-    for row_i in range(len(adjacency_list)):
-        for col in adjacency_list[row_i]:
-            tgraph[col].append(row_i)
+    tgraph = [[] for _ in range(len(adjacency_list))]   # O(v)
+    for row_i in range(len(adjacency_list)):            # O(v)
+        for col in adjacency_list[row_i]:               # O(e)
+            tgraph[col].append(row_i)                   # O(1)
     else:
         return tgraph
 
