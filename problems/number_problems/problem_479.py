ac0cccb2e674c506b43b350ba3f1f268ab9a213e
cnh867 <lukasz.osuch@motorolasolutions.com>
2020-11-13 00:47:40 +0100

    Problem #479 - O(n*n!) recursive with sorting

--- problems/number_problems/problem_479.py
+++ problems/number_problems/problem_479.py
@@ -1,20 +1,27 @@
-from typing import Union, List, Any
+from typing import List, Any, Set
 
 
 # O(n*n!)
-def permutations(elements_to_permute: List[Any], current_permutation: Union[List[Any], None] = None):
-    if current_permutation is None:
-        current_permutation = []
+def permutations(elements_to_permute: List[Any], element_used: Set[Any] = None) -> List[List[Any]]:
+    if element_used is None:
+        element_used = set()
 
-    if not elements_to_permute:
-        return [current_permutation]
+    if len(elements_to_permute) == len(element_used):
+        # O(n!) in total for all runs
+        return [[]]
 
     generated_permutations = []
-    for num in elements_to_permute:  # n * (n - 1) * (n - 2) * ... * 1 = n! => O(n!)
-        next_permutation = current_permutation + [num]  # O(n)
-        remaining_nums = [i for i in elements_to_permute if i != num]  # O(n)
-        generated_permutations += permutations(remaining_nums, next_permutation)
+    for num in elements_to_permute:
+        if num in element_used:
+            continue
+        element_used.add(num)
+        for p in permutations(elements_to_permute, element_used):
+            # O(n*n!) in total for all runs
+            p.append(num)                       # O(1)
+            generated_permutations.append(p)    # O(1)
+        element_used.remove(num)
     else:
+        generated_permutations.sort()
         return generated_permutations
 
 
@@ -25,5 +32,5 @@ assert permutations([1, 2, 3, 4]) == sorted([
 ])
 assert permutations(['A', 'B']) == [['A', 'B'], ['B', 'A']]
 
-# the algorithm suscks for duplicates - mostly cause of line #15
-assert permutations(['A', 'A']) == [['A'], ['A']]
+# the algorithm suscks for duplicates - mostly cause of line #16
+assert permutations(['A', 'A']) == []
