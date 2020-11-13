dbf024fae1ac487a309f1c663ec148186b8cbe77
cnh867 <lukasz.osuch@motorolasolutions.com>
2020-11-13 23:48:09 +0100

    Problem #487 - duplicates on different levels handled

--- problems/graph_problems/problem_487.py
+++ problems/graph_problems/problem_487.py
@@ -21,33 +21,34 @@ class BinaryNode:
         return values
 
     # O(n)
-    def level(self, node_valu: Any, current: int = 1) -> Union[int, None]:
-        if self.value == node_valu:
+    def level(self, node_valu: Any, current: int = 1, below: int = 0) -> Union[int, None]:
+        if below < 1 and self.value == node_valu:
             return current
         res = None
         if self.left:
-            res = self.left.level(node_valu, current + 1)
+            res = self.left.level(node_valu, current + 1, below - 1)
         if not res and self.right:
-            res = self.right.level(node_valu, current + 1)
+            res = self.right.level(node_valu, current + 1, below - 1)
         return res
 
     # O(n)
-    def find_parent(self, node_val) -> Tuple[Union[BinaryNode, None], Union[bool, None]]:
-        if self.left and self.left.value == node_val:
-            return self, True
-        if self.right and self.right.value == node_val:
-            return self, False
+    def find_parent(self, node_val, below: int = 0) -> Tuple[Union[BinaryNode, None], Union[bool, None]]:
+        if below < 1:
+            if self.left and self.left.value == node_val:
+                return self, True
+            if self.right and self.right.value == node_val:
+                return self, False
         res = None
         left_side = None
         if self.left:
-            res, left_side = self.left.find_parent(node_val)
+            res, left_side = self.left.find_parent(node_val, below - 1)
         if not res and self.right:
-            res, left_side = self.right.find_parent(node_val)
+            res, left_side = self.right.find_parent(node_val, below - 1)
         return res, left_side
 
     # O(n)
-    def find_sibling(self, node_val: Any) -> Union[Any, None]:
-        (parent, left_side) = self.find_parent(node_val)
+    def find_sibling(self, node_val: Any, below: int = 0) -> Union[Any, None]:
+        (parent, left_side) = self.find_parent(node_val, below - 1)
         if not parent:
             return None
         elif left_side:
@@ -69,13 +70,13 @@ class BinaryNode:
         return nodes
 
     # O(n)
-    def cousins(self, node_value: Any) -> List[Any]:
-        level = self.level(node_value)
+    def cousins(self, node_value: Any, below: int = 0) -> List[Any]:
+        level = self.level(node_value, below=below)
         if level is None:
             return list()
         values_on_level = self.all_from_level(level)
         values_on_level.remove(node_value)
-        sibling_value = self.find_sibling(node_value)
+        sibling_value = self.find_sibling(node_value, below)
         if sibling_value is not None:
             values_on_level.remove(sibling_value)
         return values_on_level
@@ -130,7 +131,26 @@ assert another_tree.cousins(8) == [1, 3]
 assert another_tree.cousins(1) == [7, 8]
 assert another_tree.cousins(3) == [7, 8]
 
-# when duplicates are present, always higher node is taken - could be improved, too lasy to change find_parent() method :)
+# when duplicates are present on different levels
 with_duplicates = build_tree(1, 2, 3, 4, 5, 6, 2)
+
+assert with_duplicates.level(2) == 2
+assert with_duplicates.level(2, below=2) == 3
+assert with_duplicates.level(2, below=1) == 2
+assert with_duplicates.level(2, below=3) is None
+
+assert with_duplicates.find_sibling(2) == 3
+assert with_duplicates.find_sibling(2, below=0) == 3
+assert with_duplicates.find_sibling(2, below=2) == 6
+assert with_duplicates.find_sibling(2, below=3) is None
+
 assert with_duplicates.cousins(2) == []
+assert with_duplicates.cousins(2, below=1) == []
+assert with_duplicates.cousins(2, below=2) == [4, 5]
+assert with_duplicates.cousins(2, below=3) == []
+
 assert with_duplicates.cousins(4) == [6, 2]
+
+# when duplicates are present on the same level, the left-most value is always used
+with_duplicates = build_tree(1, 2, 3, 4, 5, 6, 5)
+assert with_duplicates.cousins(5) == [6, 5]
