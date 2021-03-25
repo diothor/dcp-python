ce2492b74729bcaf4a8ecfb2387048a7f633af61
cnh867 <lukasz.osuch@motorolasolutions.com>
2021-03-25 23:36:01 +0100

    Problem #619 - O(n^2)

--- problems/graph_problems/problem_619.py
+++ problems/graph_problems/problem_619.py
@@ -1,20 +1,20 @@
 from itertools import product
 
 
-def neighbours(size_y, size_x, y, x) -> list:
+def neighbours(y: int, x: int, nrow: int, ncol: int) -> list:
     res = []
     if x > 0:  # left
         res.append((y, x - 1))
     if y > 0:  # top
         res.append((y - 1, x))
-    if x < size_x - 1:  # rigth
+    if x < ncol - 1:  # rigth
         res.append((y, x + 1))
-    if y < size_y - 1:  # bottom
+    if y < nrow - 1:  # bottom
         res.append((y + 1, x))
     return res
 
 
-def dfs(board, size_y, size_x, word, size_w, iletter, visited: set, y, x):
+def dfs(board: list, nrow: int, ncol: int, word: str, nletter: int, iletter: int, visited: set, y: int, x: int):
     if (y, x) in visited:
         return False
     else:
@@ -22,21 +22,22 @@ def dfs(board, size_y, size_x, word, size_w, iletter, visited: set, y, x):
 
     if board[y][x] != word[iletter]:
         return False
-    elif iletter == size_w - 1:
+    elif iletter == nletter - 1:
         return True
 
-    nbrs = neighbours(size_y, size_x, y, x)
+    nbrs = neighbours(y, x, nrow, ncol)
     for ny, nx in nbrs:
-        if dfs(board, size_y, size_x, word, size_w, iletter + 1, visited, ny, nx):
+        if dfs(board, nrow, ncol, word, nletter, iletter + 1, visited, ny, nx):
             return True
     else:
         return False
 
 
+# O(n^2) where n is number of letters in the board
 def exists(board: list, word: str) -> bool:
-    size_y, size_x = len(board), len(board[0])
-    for y, x in product(range(size_y), range(size_x)):  # O(n) where n is number of letters in the board
-        if dfs(board, size_y, size_x, word, len(word), 0, set(), y, x):
+    nrow, ncol = len(board), len(board[0])
+    for y, x in product(range(nrow), range(ncol)):  # O(n) where n is number of letters in the board
+        if dfs(board, nrow, ncol, word, len(word), 0, set(), y, x):  # O(n) where n is number of letters in the board
             return True
     else:
         return False
