5a563d3dc36a98f3b3728c5a909f27717390555e
cnh867 <lukasz.osuch@motorolasolutions.com>
2021-08-23 22:30:19 +0200

    Problem #759 - octet numbering corrected

--- problems/string_problems/problem_759.py
+++ problems/string_problems/problem_759.py
@@ -2,11 +2,11 @@ from typing import List
 
 
 # O(1)
-def parse_octet(octet_str: str, octet_num: int = 0) -> List[str]:
+def parse_octet(octet_str: str, octet_num: int = 1) -> List[str]:
     if not octet_str:
         return []
     if octet_str[0] == '0':
-        return [] if octet_num == 0 else ['0']
+        return [] if octet_num == 1 else ['0']
     octet_values = []
     new_value = ''
     for digit in octet_str:  # O(3)
@@ -23,13 +23,13 @@ assert parse_octet('') == []
 assert parse_octet('1') == ['1']
 assert parse_octet('9') == ['9']
 assert parse_octet('0') == []
-assert parse_octet('0', 1) == ['0']
+assert parse_octet('0', 2) == ['0']
 
 # two digits
 assert parse_octet('10') == ['1', '10']
 assert parse_octet('99') == ['9', '99']
 assert parse_octet('01') == []
-assert parse_octet('01', 1) == ['0']
+assert parse_octet('01', 2) == ['0']
 
 # three digits
 assert parse_octet('100') == ['1', '10', '100']
@@ -37,27 +37,27 @@ assert parse_octet('255') == ['2', '25', '255']
 assert parse_octet('256') == ['2', '25']
 assert parse_octet('999') == ['9', '99']
 assert parse_octet('011') == []
-assert parse_octet('011', 1) == ['0']
+assert parse_octet('011', 2) == ['0']
 assert parse_octet('001') == []
-assert parse_octet('001', 1) == ['0']
+assert parse_octet('001', 2) == ['0']
 
 
 # O(1)
 def ips(digits: str, start: int = 0) -> List[str]:
     res = []
-    frst_octet_values = parse_octet(digits[0:3], 0)
+    frst_octet_values = parse_octet(digits[0:3], 1)
     # O(3)
     for v1 in frst_octet_values:
         snd_start = len(v1)
-        snd_octet_values = parse_octet(digits[snd_start: snd_start + 3], 1)
+        snd_octet_values = parse_octet(digits[snd_start: snd_start + 3], 2)
         # O(3)
         for v2 in snd_octet_values:
             trd_start = snd_start + len(v2)
-            trd_octet_values = parse_octet(digits[trd_start: trd_start + 3], 2)
+            trd_octet_values = parse_octet(digits[trd_start: trd_start + 3], 3)
             # O(3)
             for v3 in trd_octet_values:
                 fth_start = trd_start + len(v3)
-                fth_octet_values = parse_octet(digits[fth_start: fth_start + 3], 3)
+                fth_octet_values = parse_octet(digits[fth_start: fth_start + 3], 4)
                 # O(3)
                 for v4 in fth_octet_values:
                     fth_end = fth_start + len(v4)
@@ -80,8 +80,10 @@ assert ips('256255255255') == []
 assert ips('255256255255') == []
 assert ips('255255256255') == []
 assert ips('255255255256') == []
+assert ips('1231231231231') == []
 
 # acceptance cases
 assert ips('2542540123') == ['254.25.40.123', '254.254.0.123']
 assert ips('25525511135') == ['255.255.11.135', '255.255.111.35']
 assert ips('11111011111') == ['111.110.11.111', '111.110.111.11']
+assert ips('25505011535') == []
