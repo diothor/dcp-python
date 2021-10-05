cb086b388500157247df73bd56a456c7a824fc6c
cnh867 <lukasz.osuch@motorolasolutions.com>
2021-10-06 00:05:32 +0200

    Problem #805 refactored
    Took 40 minutes

--- problems/string_problems/problem_805.py
+++ problems/string_problems/problem_805.py
@@ -1,21 +1,35 @@
 from collections import deque
+from typing import List
+
+
+def num_to_base(num: int, base: int) -> List[int]:
+    if num < 0:
+        return []
+    elif num == 0:
+        return [0]
+
+    bits = deque()
+    while num > 0:
+        bits.appendleft(num % base)
+        num = num // base
+    else:
+        return list(bits)
+
+
+def skip_zero(bits: List[int], base: int) -> None:
+    for index in range(len(bits) - 1, 0, -1):
+        if bits[index] < 1:
+            bits[index - 1] -= 1
+            bits[index] += base
 
 
 def alphabetical_id(col: int) -> str:
-    char_offset = 64
     char_count = 26
-    letters = deque()
+    base26bits = num_to_base(col, char_count)
+    skip_zero(base26bits, char_count)
 
-    while col > 0:
-        multiple, rest = divmod(col, char_count)
-        if rest == 0:
-            letters.appendleft('Z')
-            multiple -= 1
-        else:
-            letters.appendleft(chr(char_offset + rest))
-        col = multiple
-    else:
-        return ''.join(letters)
+    char_offset = 64
+    return ''.join(map(lambda b: chr(char_offset + b), filter(lambda b: b > 0, base26bits)))
 
 
 assert alphabetical_id(1) == 'A'
@@ -26,8 +40,8 @@ assert alphabetical_id(25) == 'Y'
 assert alphabetical_id(26) == 'Z'
 
 assert alphabetical_id(28) == 'AB'
-assert alphabetical_id(2*26) == 'AZ'
-assert alphabetical_id(2*26+1) == 'BA'
+assert alphabetical_id(2 * 26) == 'AZ'
+assert alphabetical_id(2 * 26 + 1) == 'BA'
 
 assert alphabetical_id(26 * 26 * 2 + 26 * 4 + 3) == 'BDC'
 
